cdbd7c9f2797c8f2c4ff312a9a456094938fb3b5
Charles <charlesmuller@rede.ulbra.br>
2023-03-29 22:09:09 -0300

    Ajustando css e informações na tela

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -7,7 +7,7 @@ class Login extends Component {
             <div className="usuario-container">
                 <form>
                     <h1>Acessar Seção de Planning</h1>
-                    <div>
+                    <div className="input-container">
                         <label>Digite seu usuário</label>
                         <input type="email" id="email" name="email" />
                     </div>
