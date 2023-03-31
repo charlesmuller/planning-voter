dfd667fd7f19e24619a7e90e33d3638ca948b884
Charles <charlesmuller@rede.ulbra.br>
2023-03-31 17:46:14 -0300

    ajustes na tela inicial

---
+++ src/componentes/Utils.js
@@ -0,0 +1,9 @@
+export function gerarStringAleatoria(tamanho) {
+    let caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let resultado = '';
+    for (let i = 0; i < tamanho; i++) {
+      resultado += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    }
+    return resultado;
+  }
+  
